dd8e61e9ff267ad5aeaef1e337797b772c06718c
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-16 13:41:10 +0100

    Add initial styling to the subheading

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -6,10 +6,14 @@ const Title = () => (
     <Typography variant="h1" textAlign="center" m={0}>
       Recipe Builder
     </Typography>
-    <Typography variant="h2" textAlign="center" m={0} mt={10}>
+    <Typography variant="h2" textAlign="left" fontSize={22} fontWeight="normal" lineHeight={1.5} m={0} mt={20}>
     Want to cook something nice and simple but don't have any idea where to start?
+    <br />
+    <br />
     With Recipe Builder finding a recipe was never easier.
-    Just take a picture of the ingredients you have and in a few moments you will receive a list recommended recipes
+    <br />
+    <br />
+    Just take a picture of the ingredients you have and in a few moments you will receive a list recommended recipes.
     </Typography>
   </Box>
 );
