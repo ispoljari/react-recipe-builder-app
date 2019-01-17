1345da5ec3ce1a7aebcef14fbbf5e21eee472711
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-17 09:01:38 +0100

    Implement Form FC as CC

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Input } from '@smooth-ui/core-sc';
 import * as Styled from './Form.style';
 
-const Form = () => (
-  <Styled.Form>
-    <Input type="text" name="ingredients" placeholder="Enter some ingredients..." required size="md" control p={10}/>
-  </Styled.Form>
-);
+class Form extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    this.loadRecipes();
+  }
+
+  loadRecipes = () => {
+    console.log('hello');
+  }
+
+  render() {
+    return (
+      <Styled.Form onSubmit={this.handleSubmit}>
+        <Input type="text" name="ingredients" placeholder="Enter some ingredients..." required size="md" control p={10}/>
+      </Styled.Form>
+    ); 
+  }
+};
 
 export default Form;
