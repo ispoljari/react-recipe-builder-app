594b83a2c3be8525c43eb69d4796d7dba6b60198
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-18 13:14:02 +0100

    Add smoke render test for Results component

---
+++ src/components/Results/Results.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Results from './Results';
+
+describe('<Results />', () => {
+  it('Renders without crashing', () => {
+    shallow(<Results />);
+  });
+});
