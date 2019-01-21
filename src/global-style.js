ac2374aca86ed770c020e5b7eedcf91b0ce45b84
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-21 18:10:05 +0100

    Remove 100% height on html, body

--- src/global-style.js
+++ src/global-style.js
@@ -2,9 +2,4 @@ import { globalStyle, createGlobalStyle } from '@smooth-ui/core-sc';
 
 export const Global = createGlobalStyle`
   ${globalStyle()}
-  {
-    html, body {
-      height: 100%;
-    }
-  }
 `;
