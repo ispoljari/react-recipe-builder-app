8551512fb8f939f460acdbcfa8ae799ab07a4ed2
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-21 15:41:04 +0100

    Add subtitle

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -7,7 +7,7 @@ const Title = () => (
       Recipe Builder
     </Typography>
     <Typography variant="h2" textAlign="center" fontSize={20} fontWeight="normal" lineHeight={1.5} m={0} px={18} mt={10} letterSpacing={1.5}>
-      Enter subtitles here ...
+      Finding recipes has never been easier. First, take a picture of the ingredients you whish to use. After they have been recognized by the app, simply press Search and wait for the results. You can also easily add or remove ingredients manually.
     </Typography>
   </Box>
 );
