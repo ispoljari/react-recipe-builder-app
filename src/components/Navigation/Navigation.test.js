71240dd88a63eeadf7abfd56638bd43e2e66e50e
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-22 11:28:25 +0100

    Add somke test for Navigation component

---
+++ src/components/Navigation/Navigation.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Navigation from './Navigation';
+
+describe('<Navigation />', () => {
+  it('Renders without crashing', () => {
+    shallow(<Navigation />);
+  });
+});
