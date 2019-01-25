ccf836f6c1c03a7894526d747b0a27398655e6e6
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-25 13:14:48 +0100

    Refactor Search recipes SFC to use implicit return

--- src/components/SearchRecipes/SearchRecipes.js
+++ src/components/SearchRecipes/SearchRecipes.js
@@ -3,20 +3,18 @@ import React from 'react';
 import { Button } from '@smooth-ui/core-sc';
 import * as Styled from './SearchRecipes.style';
 
-const SearchRecipes = ({handleSubmit}) => {
-  return (
-    <Styled.Form 
-    onSubmit={handleSubmit}>
-      <Button 
-      variant="success" 
-      width={1} 
-      minHeight={40} 
-      fontSize={20} 
-      type="submit">
-        Search
-      </Button>
-    </Styled.Form>
-  ); 
-};
+const SearchRecipes = ({handleSubmit}) => (
+  <Styled.Form 
+  onSubmit={handleSubmit}>
+    <Button 
+    variant="success" 
+    width={1} 
+    minHeight={40} 
+    fontSize={20} 
+    type="submit">
+      Search
+    </Button>
+  </Styled.Form>
+); 
 
 export default SearchRecipes;
