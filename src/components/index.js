583d2ef8e914a5fbc3511571fba78d737ba2bc04
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-25 14:54:17 +0100

    Allow js extensions for jsx in eslintrc

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
-export {default as Title} from './Title/Title';
-export {default as SearchRecipes} from './SearchRecipes/SearchRecipes';
-export {default as IngredientsList} from './IngredientsList/IngredientsList';
-export {default as InputIngredient} from './InputIngredient/InputIngredient';
-export {default as Results} from './Results/Results';
-export {default as Navigation} from './Navigation/Navigation';
-export {default as CaptureImg} from './CaptureImg/CaptureImg';
+export { default as Title } from './Title/Title';
+export { default as SearchRecipes } from './SearchRecipes/SearchRecipes';
+export { default as IngredientsList } from './IngredientsList/IngredientsList';
+export { default as InputIngredient } from './InputIngredient/InputIngredient';
+export { default as Results } from './Results/Results';
+export { default as Navigation } from './Navigation/Navigation';
+export { default as CaptureImg } from './CaptureImg/CaptureImg';
