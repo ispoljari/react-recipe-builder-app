9997c3ef692633c2c42e6d60842a5bae91ac4b3d
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-25 15:36:17 +0100

    Install memoize-one

--- src/components/InputIngredient/InputIngredient.js
+++ src/components/InputIngredient/InputIngredient.js
@@ -2,22 +2,25 @@ import React from 'react';
 import { Input } from '@smooth-ui/core-sc';
 import PropTypes from 'prop-types';
 
-const InputIngredient = ({ onChange, onKeyDown, value }) => (
-  <Input
-    type="text"
-    name="ingredients"
-    placeholder="eggs, bacon, salt,"
-    value={value}
-    onChange={onChange}
-    onKeyDown={onKeyDown}
-    required
-    size="md"
-    borderRadius={5}
-    fontSize={20}
-    control
-    p={10}
-  />
-);
+const InputIngredient = ({ onChange, onKeyDown, value }) => {
+  console.log('Hello');
+  return (
+    <Input
+      type="text"
+      name="ingredients"
+      placeholder="eggs, bacon, salt,"
+      value={value}
+      onChange={onChange}
+      onKeyDown={onKeyDown}
+      required
+      size="md"
+      borderRadius={5}
+      fontSize={20}
+      control
+      p={10}
+    />
+  );
+});
 
 InputIngredient.propTypes = {
   onChange: PropTypes.func.isRequired,
