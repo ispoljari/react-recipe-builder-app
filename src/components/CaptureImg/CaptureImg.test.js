fd7fd2d6592f74d24074aaf06ea9e59f487e6489
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-29 20:04:19 +0100

    Fix existing tests

--- src/components/CaptureImg/CaptureImg.test.js
+++ src/components/CaptureImg/CaptureImg.test.js
@@ -4,6 +4,21 @@ import CaptureImg from './CaptureImg';
 
 describe('<CaptureImg />', () => {
   it('Renders without crashing', () => {
-    shallow(<CaptureImg />);
+    const onClick = jest.fn();
+    const onChange = jest.fn();
+    const capturedImg = "dummyString";
+    const loadingPredictions = true;
+    const loadingRecipes= true;
+    const setRef = {};
+
+    shallow(
+      <CaptureImg 
+        onClick={onClick}
+        onChange={onChange}
+        capturedImg={capturedImg}
+        loadingPredictions={loadingPredictions}
+        loadingRecipes={loadingRecipes}
+        setRef={setRef}
+      />);
   });
 });
